45a266195001a40bc924b594732864c3190c5b11
Ba7arathinam <bala01225@gmail.comm>
2023-07-19 15:13:34 +0530

    re do someting on gitignore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,createContext } from "react";
 import ToDo from "./components/ToDo";
 import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { purple } from '@mui/material/colors';
 
-
+export const ThemeContext= createContext(null);
 function App() {
-
+  
   const [toDo, setToDo] = useState([])
   const [text, setText] = useState("")
   const [isUpdating, setIsUpdating] = useState(false)
   const [toDoId, setToDoId] = useState("")
+  const[theme,setTheme]=useState("dark");
+
 
   useEffect(() => {
     getAllToDo(setToDo)
@@ -20,8 +25,55 @@ function App() {
     setToDoId(_id)
   }
 
+  const toggleTheme =()=>{
+    setTheme((curr)=>(curr ==="light" ? "dark":"light"))
+  }
+  const BootstrapButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: '#0063cc',
+    borderColor: '#0063cc',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  });
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  }));
   return (
-    <div className="App">
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+    <div className="App" id={theme}>
 
       <div className="container">
 
@@ -34,7 +86,10 @@ function App() {
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
-
+          
+          <ColorButton variant="contained"  onClick={isUpdating ?
+              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+              : () => addToDo(text, setText, setToDo)}> {isUpdating ? "Update" : "Add"}</ColorButton>
           <div
             className="add"
             onClick={isUpdating ?
@@ -58,6 +113,7 @@ function App() {
       </div>
 
     </div>
+    </ThemeContext.Provider>
   );
 }
 
